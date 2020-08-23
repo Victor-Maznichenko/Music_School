b164b54f1d2aeea8bf4c8c34280847d747526dc0
Victor-Maznichenko <66438596+Victor-Maznichenko@users.noreply.github.com>
2020-08-23 22:10:45 +0300

    312k
    123d

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(function(){
+	
+	AOS.init({
+		disable: function () {
+			var maxWidth = 1000;
+			return window.innerWidth < maxWidth;
+		},
+		throttleDelay: 99
+	});
+	
+	$('.menu-btn').on('click', function(){
+		$(this).toggleClass('menu-btn--active');
+		$('.menu-mobile').toggleClass('menu-mobile--active');
+	});
+	
+});
