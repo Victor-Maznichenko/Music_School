8247dc3225425395065a92e0bc320eb0a82d3a1a
Victor-Maznichenko <66438596+Victor-Maznichenko@users.noreply.github.com>
2020-08-23 22:26:22 +0300

    цуау
    фып2

--- js/main.js
+++ js/main.js
@@ -1,11 +1,4 @@
 $(function(){
-
-		disable: function () {
-			var maxWidth = 1000;
-			return window.innerWidth < maxWidth;
-		},
-		throttleDelay: 99
-	});
 	
 	$('.menu-btn').on('click', function(){
 		$(this).toggleClass('menu-btn--active');
